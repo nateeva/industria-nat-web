cbf9686c2900924bfaa6f76742aa0924271c4921
nateeva <nataliaemerlo@gmail.com>
2024-09-23 17:15:15 -0300

    scroll al inicio en al dirigirse a ruta tienda

--- src/components/pages/Tienda.jsx
+++ src/components/pages/Tienda.jsx
@@ -2,8 +2,13 @@ import { SliderProduct } from "../tienda/SliderProduct"
 import { NavBar } from "../NavBar"
 import { Footer } from "../Footer"
 import { BannerTienda } from "../tienda/BannerTienda"
+import { useEffect } from "react"
 
 export const Tienda = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <NavBar isTienda={true} />
